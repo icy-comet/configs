35a765229ba609cc7924144758c5c5c86f69d85a
Aniket Teredesai <aniket.teredesai@gmail.com>
2021-09-23 10:11:04 +0530

    add a stat alias
    the alias makes it easy to see octal file/folder permissions in terminal

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -1,4 +1,7 @@
-# the file lives in `ZSH_CUSTOM` dir defined in a ENV variable (in `.zshrc`) before loading `oh-my-zsh`
+# the file lives in `ZSH_CUSTOM` dir defined in a ENV variable (in `.zshrc`) before loading `oh-my-zsh`; default `~/.oh-my-zsh/custom/`
 
 alias py="python"
 alias cls="clear"
+
+# makes getting octal file/folder permissions easy
+alias catmod="stat -c '%A %a %n'"
