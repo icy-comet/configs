66883ea82b7f4fa8fa9be3e19d08e6adb0011647
Aniket Teredesai <aniket.teredesai@gmail.com>
2021-10-11 13:10:56 +0530

    update vscode settings, terminal font & git settings

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -5,3 +5,8 @@ alias cls="clear"
 
 # makes getting octal file/folder permissions easy
 alias catmod="stat -c '%A %a %n'"
+
+# fix permissions for all files & folders copied from Windows/NTFS drives
+# takes the directory to be fixed after this
+# e.g. `> fixperms /home/aniket/Projects`
+alias fixperms="chmod -R u=rw,og=r,+X"
