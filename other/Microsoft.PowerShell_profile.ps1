586e68ca12317bf31f3929a33ca88a5ced15091f
Aniket Teredesai <aniket.teredesai@gmail.com>
2022-05-07 06:54:01 +0530

    update powershell profile

--- other/Microsoft.PowerShell_profile.ps1
+++ other/Microsoft.PowerShell_profile.ps1
@@ -2,14 +2,5 @@
 # Use `echo $PROFILE` to find the exact path on the system
 
 Invoke-Expression (&starship init powershell)
-Function ManageEnv {
-    If ($args[0]) {
-        Get-ChildItem -Path "env:$($args[0])" | Select-Object -ExpandProperty Value
-    }
-    Else {
-        Get-ChildItem -Path env:
-    }
-}
 $env:VIRTUAL_ENV_DISABLE_PROMPT = 1
 Set-Alias -Name ls -Value lsd
-Set-Alias -Name printenv -Value ManageEnv
