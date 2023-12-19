030cbe0eec2e0c0f7444fc411932c2ea355b134f
Aniket Teredesai <aniket.teredesai@gmail.com>
2023-12-19 21:54:35 +0530

    utils: fix progressbar script
    - double quote vars to avoid globbing
    - rename vars
    - set volume limit to 100%

--- linux/home/.config/utils/loud_light_control.sh
+++ linux/home/.config/utils/loud_light_control.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 
+barCount=$(eww get barCount)
 entity="$1"
 verb="$2"
 barIcon=""
 barPercent=""
 
 function openWindow() {
-    eww update barIcon="$barIcon" barPercent="$barPercent" && eww open overlaybar
-    barPresses=$(eww get barPresses)
-    barPresses=$((barPresses + 1))
-    eww update barPresses="$barPresses"
+    eww update barIcon="$barIcon" barPercent="$barPercent" && eww open overlayWindow
+    barCount=$((barCount + 1))
+    eww update barCount="$barCount"
 
     sleep 1
 
-    barPresses=$(eww get barPresses)
-    barPresses=$((barPresses - 1))
-    eww update barPresses="$barPresses"
-    if [ $barPresses -lt 1 ]; then
-        eww close overlaybar
+    barCount=$(eww get barCount)
+    barCount=$((barCount - 1))
+    eww update barCount="$barCount"
+    if [ $barCount -lt 1 ]; then
+        eww close overlayWindow
     fi
 }
 
-if [ $entity == "volume" ]; then
-    if [ $verb == "up" ]; then
-        pactl set-sink-volume @DEFAULT_SINK@ +5%
-        barIcon="󰝝"
-    elif [ $verb == "down" ]; then
+if [ "$entity" == "volume" ]; then
+    barPercent=$(pactl get-sink-volume @DEFAULT_SINK@ | head -n 1 | awk '{print substr($5, 1, length($5)-1)}')
+    if [ "$verb" == "up" ]; then
+        if [ "$barPercent" -lt 100 ]; then
+            pactl set-sink-volume @DEFAULT_SINK@ +5%
+            barIcon="󰝝"
+        else
+            exit 1
+        fi
+    elif [ "$verb" == "down" ]; then
         pactl set-sink-volume @DEFAULT_SINK@ -5%
         barIcon="󰝞"
-    elif [ $verb == "mute" ]; then
+    elif [ "$verb" == "mute" ]; then
         pactl set-sink-mute @DEFAULT_SINK@ toggle
         barIcon="󰝟"
         if [ "$(pactl get-sink-mute @DEFAULT_SINK@ | cut --delimiter=" " -f 2)" == "no" ]; then
@@ -38,10 +43,10 @@ if [ $entity == "volume" ]; then
         exit 1
     fi
     barPercent=$(pactl get-sink-volume @DEFAULT_SINK@ | head -n 1 | awk '{print substr($5, 1, length($5)-1)}')
-elif [ $entity == "brightness" ]; then
-    if [ $verb == "up" ]; then
+elif [ "$entity" == "brightness" ]; then
+    if [ "$verb" == "up" ]; then
         brightnessctl set 5%+
-    elif [ $verb == "down" ]; then
+    elif [ "$verb" == "down" ]; then
         brightnessctl set 5%-
     else
         exit 1
