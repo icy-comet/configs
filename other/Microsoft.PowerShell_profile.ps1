3ff62b2af3de124a26fa64c961fe22ff101718f9
Aniket Teredesai <aniket.teredesai@gmail.com>
2026-04-21 09:18:06 +0530

    add uv to ps profile

--- other/Microsoft.PowerShell_profile.ps1
+++ other/Microsoft.PowerShell_profile.ps1
@@ -6,26 +6,29 @@ fnm env --use-on-cd | Out-String | Invoke-Expression
 # oh-my-posh init pwsh --config .ohmyposhtheme.json | Invoke-Expression
 
 $env:VIRTUAL_ENV_DISABLE_PROMPT = 1
-$env:LESSCHARSET='utf-8'
+$env:LESSCHARSET = 'utf-8'
 
 # The function emulates the `ln` command in *NIX for making symbolic links
 Function ln {
 
-  If (($args[0] -eq "-s" -or $args[0] -eq "--symbolic") -and $args.length -eq 3) {
-    New-Item -ItemType SymbolicLink -Path $args[2] -Target $args[1]
-    Return
-  } ElseIf ($args.length -eq 2) {
-    New-Item -ItemType HardLink -Path $args[1] -Target $args[0]
-    Return
-  }
+    If (($args[0] -eq "-s" -or $args[0] -eq "--symbolic") -and $args.length -eq 3) {
+        New-Item -ItemType SymbolicLink -Path $args[2] -Target $args[1]
+        Return
+    }
+    ElseIf ($args.length -eq 2) {
+        New-Item -ItemType HardLink -Path $args[1] -Target $args[0]
+        Return
+    }
 
-  Write-Error -Message "Insufficient/Invalid arguments provided." -Category "InvalidArgument"
+    Write-Error -Message "Insufficient/Invalid arguments provided." -Category "InvalidArgument"
 
 }
 
 Function ll {
-  param (
-    $Location
-  )
-  lsd -l -a $Location
+    param (
+        $Location
+    )
+    lsd -l -a $Location
 }
+
+(& uv generate-shell-completion powershell) | Out-String | Invoke-Expression
